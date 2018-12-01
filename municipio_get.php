7b0fd0eb1e8e2dd93e8d4320ec1068d3beebf655
elShunior <zahoz.moopa@gmail.com>
2018-12-01 03:01:20 -0500

    UBIGEO TOTAL
    completo ubigeo :V
    pinshe wada

--- municipio_get.php
+++ municipio_get.php
@@ -2,11 +2,11 @@
 	include("conexion.php");
 	$id_departamento=intval($_REQUEST['id_departamento']);
 	$municipios = $conn->prepare("SELECT * FROM ubprovincia WHERE idDepa = '$id_departamento'") or die(mysqli_error());
-		echo '<option value = ""> Selecciona un municipio:  </option>';
+		echo '<option value = "">PROVINCIA</option>';
 	if($municipios->execute()){
 		$a_result = $municipios->get_result();
 	}
 		while($row = $a_result->fetch_array()){
-			echo '<option value = "'.$row['ubProv'].'">'.utf8_encode( $row['provincia']).'</option>';
+			echo '<option value = "'.$row['idProv'].'">'.utf8_encode( $row['provincia']).'</option>';
 		}
 ?>
